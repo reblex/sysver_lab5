1eefa9eeff889e2c601a97b7347315aa76f23fba
Anders Eriksson <Anders_Eriksson_93@hotmail.se>
2018-05-10 11:22:17 +0200

    test: User story 6

--- test/TestBowling.java
+++ test/TestBowling.java
@@ -67,4 +67,21 @@ public class TestBowling {
         bg.addScore(new BowlFrame(2,6));
         assertEquals(94, bg.getScore());
     }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void BowlingGame_getScore_validGameWithSpare_return88() {
+        BowlingGame bg = new BowlingGame();
+        bg.addScore(new BowlFrame(1,9));
+        bg.addScore(new BowlFrame(3,6));
+        bg.addScore(new BowlFrame(7,2));
+        bg.addScore(new BowlFrame(3,6));
+        bg.addScore(new BowlFrame(4,4));
+        bg.addScore(new BowlFrame(5,3));
+        bg.addScore(new BowlFrame(3,3));
+        bg.addScore(new BowlFrame(4,5));
+        bg.addScore(new BowlFrame(8,1));
+        bg.addScore(new BowlFrame(2,6));
+        assertEquals(88, bg.getScore());
+    }
 }
