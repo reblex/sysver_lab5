4a72d296c6917723299531df2bbb72ba42ac23f8
Anders Eriksson <Anders_Eriksson_93@hotmail.se>
2018-05-10 09:22:43 +0200

    feat: User story 1

---
+++ src/BowlFrame.java
@@ -0,0 +1,13 @@
+public class BowlFrame {
+    private static int score1;
+    private static int score2;
+
+    public BowlFrame(int s1, int s2) {
+        score1 = s1;
+        score2 = s2;
+    }
+    public int[] getFrame(){
+        int arr[] = {score1, score2};
+        return arr;
+    }
+}
--- test/TestBowling.java
+++ test/TestBowling.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestSorting {
+public class TestBowling {
 
     @SuppressWarnings("deprecation")
     @Test
