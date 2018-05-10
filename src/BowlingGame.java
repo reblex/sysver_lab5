3a500192a76816f5eb2b77953df869eda9b873e6
Anders Eriksson <Anders_Eriksson_93@hotmail.se>
2018-05-10 10:06:42 +0200

    feat: User story 3

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,2 +1,12 @@
 public class BowlingGame {
+    private static BowlFrame[] bf = new BowlFrame[10];
+
+    public BowlingGame(){
+        for(int i = 0; i < 10; i++){
+            bf[i] = new BowlFrame();
+        }
+    }
+    public BowlFrame[] getFrames(){
+        return bf;
+    }
 }
